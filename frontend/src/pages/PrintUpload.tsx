a66208523d9a12962676fc21d8a6deea5bf1dbf0
thudngm <dngmtthu@gmail.com>
2024-11-11 16:39:58 +0700

    Fix display on PrintUpload page

--- frontend/src/pages/PrintUpload.tsx
+++ frontend/src/pages/PrintUpload.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 import type { UploadProps } from 'antd';
 import { message, Upload } from 'antd';
+import '../pages/PrintUpload.css'
 
 const { Dragger } = Upload;
 
@@ -28,18 +29,20 @@ const props: UploadProps = {
 
 const PrintUpload: React.FC = () => (
     <MainLayout>
-    <div className='p-10'>
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-          banned files.
-        </p>
-      </Dragger>
-    </div>
+      <div className='col-flex p-10'>
+        <div className=''>
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+              banned files.
+            </p>
+          </Dragger>
+        </div>
+      </div>
     </MainLayout>
 );
 
