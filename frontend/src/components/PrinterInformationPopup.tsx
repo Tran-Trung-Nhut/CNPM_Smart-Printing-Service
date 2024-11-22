31bcac8d7bfac0259e14479a6b444455f260d854
Tran-Trung-Nhut <ttnhut904@gmail.com>
2024-11-22 21:54:13 +0700

    update some page

---
+++ frontend/src/components/PrinterInformationPopup.tsx
@@ -0,0 +1,32 @@
+export default function PrinterInformationPopup({ onClose }: { onClose: () => void }) {
+    return (
+        <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+            onClick={onClose} // Đóng popup khi bấm vào backdrop
+        >
+            <div
+                className="bg-white p-6 rounded-sm shadow-lg w-96 text-center space-y-4 font-mono"
+                onClick={(e) => e.stopPropagation()} // Ngăn sự kiện lan truyền
+            >
+                <div className="flex justify-start">
+                    <p className="text-blue-400">Đăng nhập với tư cách:</p>
+                </div>
+                <div>
+                    <button
+                        className="border-[1px] rounded w-full hover:bg-gray-300 py-1"
+                        onClick={onClose}
+                    >
+                        Sinh viên
+                    </button>
+                    <button
+                        className="border-[1px] rounded w-full hover:bg-gray-300 py-1"
+                        onClick={onClose}
+                    >
+                        SPSO
+                    </button>
+                </div>
+                <div className="w-full max-w-sm border-t border-gray-300 mt-4"></div>
+            </div>
+        </div>
+    );
+}
--- frontend/src/pages/Student.tsx
+++ frontend/src/pages/Student.tsx
@@ -1,4 +1,7 @@
 import "primeicons/primeicons.css";
+import { useState } from "react";
+import StudentInformationPopup from "../components/StudentInformationPopup";
+
 const datas = [
     { name: "Trần Trung Nhựt", MSSV: "2212483", numberOfPrinting: 354, pages: 102 },
     { name: "Lâm Bảo Minh", MSSV: "2212484", numberOfPrinting: 120, pages: 58 },
@@ -6,36 +9,55 @@ const datas = [
 ];
 
 export default function Student() {
-    return(
-        <div className="overflow-x-auto flex flex-col"> 
-            <table className="border-[1px] rounded mt-5 mx-5 bg-white">
-                <thead className="bg-[#C6DCFE]">
-                    <tr className="">
-                        <th className="px-4 py-2"></th>
-                        <th className="px-4 py-2">Họ và tên</th>
-                        <th className="px-4 py-2">MSSV</th>
-                        <th className="px-4 py-2">Tổng số lần in</th>
-                        <th className="px-4 py-2">Số trang còn lại</th>
-                        <th className="px-4 py-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {datas.map((data, index) => (
-                        <tr key={index} className={`${index % 2 === 0 ? "" : "bg-gray-100"}`}>
-                            <td className="px-4 py-2 text-center">{index + 1}</td>
-                            <td className="px-4 py-2 text-center">{data.name}</td>
-                            <td className="px-4 py-2 text-center">{data.MSSV}</td>
-                            <td className="px-4 py-2 text-center">{data.numberOfPrinting}</td>
-                            <td className="px-4 py-2 text-center">{data.pages}</td>
-                            <td className="px-4 py-2 text-center">
-                                <button className="text-gray-400">
-                                    <i className="pi pi-info-circle"/>
-                                </button>
-                            </td>
+    const [isShowInformation, setIsShowInformation] = useState<boolean>(false)
+    return (
+        <div className="overflow-x-auto shadow-xl rounded flex flex-col justify-between items-center min-h-screen bg-white mt-5 mx-5">
+            {isShowInformation && (
+                <StudentInformationPopup onClose={() => setIsShowInformation(false)}/>
+            )}
+            
+            <div className="w-full">
+                <div className="bg-[#C6DCFE] flex items-center space-x-1">
+                    <div className="pl-2 py-2 rounded">
+                        <input placeholder="Nhập họ và tên hoặc MSSV" className="w-64 pl-1 rounded placeholder:italic"/>
+                    </div>
+                    <i className="pi pi-search"/>
+                </div>
+                <table className="table-auto w-full bg-white">
+                    <thead className="bg-[#C6DCFE]">
+                        <tr>
+                            <th className="px-4 py-2 w-10">#</th>
+                            <th className="px-4 py-2">Họ và tên</th>
+                            <th className="px-4 py-2">MSSV</th>
+                            <th className="px-4 py-2">Tổng số lần in</th>
+                            <th className="px-4 py-2">Số giấy còn lại</th>
+                            <th className="px-4 py-2">Hành động</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="">
+                        {datas.map((data, index) => (
+                            <tr key={index} className={index % 2 === 0 ? "" : "bg-gray-100"}>
+                                <td className="px-4 py-2 text-center">{index + 1}</td>
+                                <td className="px-4 py-2 text-center">{data.name}</td>
+                                <td className="px-4 py-2 text-center">{data.MSSV}</td>
+                                <td className="px-4 py-2 text-center">{data.numberOfPrinting}</td>
+                                <td className="px-4 py-2 text-center">{data.pages}</td>
+                                <td className="px-4 py-2 text-center">
+                                    <button 
+                                    type="button"
+                                    className="text-gray-400"
+                                    onClick={() => setIsShowInformation(true)}>
+                                        <i className="pi pi-info-circle"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="bg-[#C6DCFE] h-12 flex items-center justify-start w-full rounded">
+                <p className="pl-2">Tổng số trang: {datas.length}</p>
+            </div>
         </div>
-    )
+    );
 }
