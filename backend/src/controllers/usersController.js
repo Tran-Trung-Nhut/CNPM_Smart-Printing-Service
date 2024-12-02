8eefa7b94494926e709736bc64f73fc230c2fdd1
HuyHaloed <152784240+HuyHaloed@users.noreply.github.com>
2024-12-02 16:41:07 +0700

    Bổ sung hàm update paper all

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -57,6 +57,41 @@ exports.createUser = async (req, res) => {
     }
 };
 
+exports.allAddPaper = async (req, res) => {
+    const { role } = req.query;
+    const { paperAmount } = req.params;
+
+    if (role !== 'student') {
+        return res.status(400).json({ status: 400, message: "Invalid role. Please provide query with role 'student'." });
+    }
+    const amount = parseInt(paperAmount, 10);
+    if (isNaN(amount) || amount <= 0) {
+        return res.status(400).json({ status: 400, message: "Invalid paper amount. It must be a positive number." });
+    }
+    try {
+        const students = await usersModel.getALLUsers({ role: 'student' });
+        if (!students.length) {
+            return res.status(404).json({ status: 404, message: "No students found." });
+        }
+        await Promise.all(students.map(student => 
+            usersModel.updateUser(student.user_ID, { pageBalance: student.pageBalance + amount })
+        ));
+        return res.status(200).json({ 
+            status: 200, 
+            message: `${students.length} students updated with ${amount} papers added successfully!` 
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ 
+            status: 500, 
+            message: 'Error updating paper count for students', 
+            error: error.message 
+        });
+    }
+};
+
+
+
 // exports.login = async (req, res) => {
 //     const { email, password } = req.body;
 //     try {
